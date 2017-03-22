5148f38aa16cb6e9c234efa7b894bb461f2d2a36
Jay Ryu <JayRyu@cses-macbook-pro.local>
2017-03-22 14:57:16 -0500

    copied

--- CommandBasic/main.m
+++ CommandBasic/main.m
@@ -13,6 +13,38 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSLog(@"Hello, World!");
         NSLog(@"My First Commit");
+        
+        NSString *str1 = @"Hello";
+        NSString *str2 = @"World";
+        int year = 1982;
+        NSString *result = [NSString stringWithFormat:@"%@ %@ %d", str1, str2, year];
+        NSLog(@"%@", result);
+        NSLog(@"%lu", (unsigned long)result.length);
+        
+        int one = 10;
+        int two = 20;
+        
+        if (one == two) {
+            NSLog(@"YES");
+        } else {
+            NSLog(@"NO");
+        }
+        
+        
+        if([str1 isEqualToString:str2]) {
+            NSLog(@"yes");
+        }
+        
+        
+        NSNumber *num = [NSNumber numberWithBool:NO];
+        NSNumber *num1 = @(10);
+        
+        NSInteger value = [num1 integerValue];
+        
+        NSLog(@"%ld", (long)value);
+        
+        
+
     }
     return 0;
 }
