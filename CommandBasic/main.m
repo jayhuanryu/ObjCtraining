fe8810543d149925b6dce0af48d91d709679f879
Jay Ryu <JayRyu@cses-macbook-pro.local>
2017-03-22 14:34:38 -0500

    Initial Commit

---
+++ CommandBasic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommandBasic
+//
+//  Created by Jay Ryu on 3/22/17.
+//  Copyright © 2017 CSUSB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
